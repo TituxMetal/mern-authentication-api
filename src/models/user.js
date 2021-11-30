14a3b7492c5dfd22729325b3d213687e2ab08bc8
Titux Metal (DEV) <github@lgdweb.fr>
2021-11-30 09:34:53 +0100

    Adding user.js file in models; adds the user model

---
+++ src/models/user.js
@@ -0,0 +1,32 @@
+import { model, Schema } from 'mongoose'
+
+import { stringToHash } from '~/helpers'
+
+const userSchema = new Schema(
+  {
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    username: { type: String, required: true }
+  },
+  { timestamps: true }
+)
+
+userSchema.set('toJSON', {
+  versionKey: false,
+  virtuals: true,
+  transform: (doc, { _id, password, ...rest }) => rest
+})
+
+userSchema.pre('save', async function preSave(done) {
+  if (this.isModified('password')) {
+    const hash = await stringToHash(this.get('password'))
+
+    this.set('password', hash)
+  }
+
+  done()
+})
+
+const User = model('User', userSchema)
+
+export default User
