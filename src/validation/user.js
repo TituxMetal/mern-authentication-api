b72b1afa2c768cac73473ebed176561b1e27380f
Titux Metal (DEV) <github@lgdweb.fr>
2021-12-01 08:56:09 +0100

    Adding user.js file in validation; validation rules for registering new user

---
+++ src/validation/user.js
@@ -0,0 +1,48 @@
+import { body } from 'express-validator'
+
+const ucFirst = ([first, ...rest]) => [first.toUpperCase(), ...rest].join('')
+
+const requiredField = field =>
+  body(field)
+    .exists()
+    .withMessage(`${ucFirst(field)} is required`)
+
+const emailField = body('email')
+  .notEmpty()
+  .withMessage('Email must not be empty')
+  .bail()
+  .isEmail()
+  .withMessage('Email must be a valid email')
+  .optional()
+  .normalizeEmail()
+
+const passwordField = body('password')
+  .trim()
+  .notEmpty()
+  .withMessage('Password must not be empty')
+  .bail()
+  .isLength({ min: 8, max: 128 })
+  .withMessage('Password must have between 8 and 128 characters long')
+  .optional()
+
+const usernameField = body('username')
+  .trim()
+  .notEmpty()
+  .withMessage('Username must not be empty')
+  .bail()
+  .isLength({ min: 4, max: 128 })
+  .withMessage('Username must have between 4 and 128 characters long')
+  .optional()
+
+const userValidator = {
+  register: [
+    requiredField('email'),
+    emailField,
+    requiredField('password'),
+    passwordField,
+    requiredField('username'),
+    usernameField
+  ]
+}
+
+export default userValidator
