dc3019cc94040f0d7efea0a1403005146c8b619d
Titux Metal (DEV) <github@lgdweb.fr>
2021-12-01 09:34:36 +0100

    Updating auth.js file in controllers; implements the login function

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,8 +3,16 @@ import { requestValidator } from '@lgdweb/common-express-helpers'
 import { authService, userService } from '~/services'
 import { userValidator } from '~/validation'
 
-const login = (req, res) => {
-  res.status(200).json({ message: 'Auth Controller => login' })
+const login = async ({ body, session }, res) => {
+  const user = await userService.checkCredentials(body)
+  const token = await authService.getTokenAndCreateSession(session, user.id)
+  const response = {
+    info: 'POST /api/auth/login',
+    message: 'User has been successfully logged in.',
+    data: { user, token }
+  }
+
+  res.status(201).json(response)
 }
 
 const register = async ({ body, session }, res) => {
@@ -29,7 +37,7 @@ const reset = (req, res) => {
 
 export default {
   forgot,
-  login,
+  login: [requestValidator(userValidator.login), login],
   register: [requestValidator(userValidator.register), register],
   reset
 }
