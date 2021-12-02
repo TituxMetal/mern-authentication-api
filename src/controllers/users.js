938119061697d78f61171336f1677b7eeb7e6795
Titux Metal (DEV) <github@lgdweb.fr>
2021-12-02 07:34:17 +0100

    Updating users.js file in controllers; implements the getSingle function

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,9 +1,19 @@
+import { requestValidator } from '@lgdweb/common-express-helpers'
+
+import { userService } from '~/services'
+import { objectIdValidator } from '~/validation'
+
 const getAll = (req, res) => {
   res.status(200).json({ message: 'Users Controller => getAll' })
 }
 
-const getSingle = (req, res) => {
-  res.status(200).json({ message: 'Users Controller => getSingle' })
+const getSingle = async ({ params }, res) => {
+  const userId = params.userId
+  const user = await userService.details(userId)
+
+  const response = { info: 'GET /api/users/:userId', params: userId, data: user }
+
+  res.status(200).json(response)
 }
 
 const update = (req, res) => {
@@ -14,4 +24,9 @@ const remove = (req, res) => {
   res.status(200).json({ message: 'Users Controller => remove' })
 }
 
-export default { getAll, getSingle, remove, update }
+export default {
+  getAll,
+  getSingle: [requestValidator(objectIdValidator('userId')), getSingle],
+  remove,
+  update
+}
