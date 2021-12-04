31c95eaaee2dcfdac81284c64e623181725bc00a
Titux Metal (DEV) <github@lgdweb.fr>
2021-12-04 09:36:26 +0100

    Updating users.js file in routes; uses the isAuthenticated middleware on update route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 
 import { usersController } from '~/controllers'
+import { isAuthenticated } from '~/middlewares'
 
 const router = new Router()
 
 router.get('/', usersController.getAll)
 router.get('/:userId', usersController.getSingle)
-router.put('/:userId', usersController.update)
+router.put('/:userId', isAuthenticated, usersController.update)
 router.delete('/:userId', usersController.remove)
 
 export default router
