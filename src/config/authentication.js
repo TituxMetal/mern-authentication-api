08857e2f9901a7e4efafae4fb3ec81f03873c332
Titux Metal (DEV) <github@lgdweb.fr>
2021-12-05 18:05:18 +0100

    Updating authentication.js file in config; adds a reset token expire variable

--- src/config/authentication.js
+++ src/config/authentication.js
@@ -3,3 +3,5 @@ const { JWT_SECRET = 'jwt secret key' } = process.env
 export const CRYPTO_WORK_FACTOR = 12
 
 export const jwtSecret = JWT_SECRET
+
+export const RESET_TOKEN_EXPIRE = 10 * (60 * 1000) // Ten Minutes
