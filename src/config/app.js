3c4ac2af8aed86fc770f33e9f5d39179fbc4271f
Titux Metal (DEV) <github@lgdweb.fr>
2021-12-06 06:56:57 +0100

    Updating app.js file in config; adds host url variable

--- src/config/app.js
+++ src/config/app.js
@@ -1,4 +1,6 @@
-const { NODE_ENV = 'development', PORT = 5000 } = process.env
+const { HOST_URL = 'http://localhost', NODE_ENV = 'development', PORT = 5000 } = process.env
+
+export const hostUrl = HOST_URL
 
 export const inProd = NODE_ENV === 'production'
 
